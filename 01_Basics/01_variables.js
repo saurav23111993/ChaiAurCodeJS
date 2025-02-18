04270ac6809665452187cc88981860b06bd18966
saurav23111993 <106614642+saurav23111993@users.noreply.github.com>
2025-02-18 19:14:47 +0000

    Variable Const

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,7 @@
+const pivalue = 3.14;
+let accountEmail = "test@test.com";
+var accountPassword = "1234";
+accountPhone = 12434
+
+//pivalue = 55
+console.table([pivalue,accountEmail,accountPassword,accountPhone])
