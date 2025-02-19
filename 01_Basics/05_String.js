975d7f2ff1315fb9c904db99e580e8fa74f58bc9
saurav23111993 <106614642+saurav23111993@users.noreply.github.com>
2025-02-19 17:37:33 +0000

    String Changes

---
+++ 01_Basics/05_String.js
@@ -0,0 +1,15 @@
+
+let fName = "Saurav";
+let lName = "Tiwari"
+// String Interpolation Using Backticks
+let conc = `My First name is ${fName} and Last Name is ${lName}`;
+console.log(conc);
+
+
+// String 
+
+let mName = new String("Saurav");
+console.log(mName);
+
+console.log(mName[0]);
+console.log(mName.__proto__);
