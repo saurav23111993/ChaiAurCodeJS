9329134634b8dedfb7330d434d9d6bca9519c45a
saurav23111993 <106614642+saurav23111993@users.noreply.github.com>
2025-02-19 06:09:46 +0000

    DataType

---
+++ 01_Basics/02_DataType.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// alert(5+4);
+console.log("Test");
+
+console.log(typeof null); // Type is Object
+console.log(typeof undefined); // Type is Undefines
+console.log(typeof "Saurav"); //Type is String
+
