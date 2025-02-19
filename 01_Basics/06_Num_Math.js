da666c74f30d9666f1b5de7a5eb2f2ab2ae0ad39
saurav23111993 <106614642+saurav23111993@users.noreply.github.com>
2025-02-19 19:40:10 +0000

    Math

---
+++ 01_Basics/06_Num_Math.js
@@ -0,0 +1,23 @@
+const num123 = 12;
+console.log(num123);
+
+const num12 = new Number(90);
+console.log(num12);
+
+console.log(num12.toString().length);
+console.log(num12.toFixed(2));
+
+const otherNum = 23.2997;
+console.log(otherNum.toPrecision(4));
+
+const hunderes = 100000000;
+console.log(hunderes.toLocaleString("en-IN"));
+
+console.log(Number.MAX_VALUE);
+ 
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.abs(-10));
+console.log(Math.round(4.7));
+console.log(Math.ceil(4.7));
+console.log(Math.floor(4.7));
