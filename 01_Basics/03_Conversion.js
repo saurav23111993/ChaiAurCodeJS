0aa97526d6ab8a64550aef52c0ffbec208e09edb
saurav23111993 <106614642+saurav23111993@users.noreply.github.com>
2025-02-19 11:33:08 +0000

    Operators

--- 01_Basics/03_Conversion.js
+++ 01_Basics/03_Conversion.js
@@ -32,3 +32,25 @@ let someNumber = true
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof (stringNumber));
+
+
+/*******************************Operation ***********************/
+
+let value = 3;
+let negValue = -value;
+console.log(value);
+console.log(negValue);
+
+
+let x = 3;
+const y = x++;
+
+console.log("Value of x ", x,"Value of y ", y);
+
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+console.log("Value of a ", a,"Value of b ", b);
+
+// Expected output: "a:4, b:4"
