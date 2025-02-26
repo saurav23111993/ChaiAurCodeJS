58dbeff0b5ccbf57405f950c0a00948b0d2a3294
saurav23111993 <106614642+saurav23111993@users.noreply.github.com>
2025-02-26 19:10:05 +0000

    Scope

---
+++ 03_Basics/02_Scopes.js
@@ -0,0 +1,24 @@
+// let a = 10;
+// const b = 20;
+// var c = 30; 
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+//{ } --> Scopes (If Else Function)
+
+if(true){
+    let a = 10;
+    const b = 20;
+    var c = 30; 
+    d = 50;
+}
+
+//console.log(a);
+//console.log(b);
+c = 300
+console.log(c);
+console.log(d);
+
+// Block Scope and Global Scope
