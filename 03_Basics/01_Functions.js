fc950ef2d3f2cd5413199b2c00c6fde00b1c38d7
saurav23111993 <106614642+saurav23111993@users.noreply.github.com>
2025-02-26 18:51:51 +0000

    Function

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -10,6 +10,17 @@ sayMyName(); // Execute Function
 function addTwoNums (a, b){
     if(typeof a === 'number' &&  typeof b === 'number'){
     return a+b;
+    }else {
+        return 0;
     }
 }
-console.log(addTwoNums(9,8))
+console.log(addTwoNums("9",8))
+//Rest Operator
+function addAll(a,b, ...num){
+
+    console.log(...num);
+    console.log(a);
+    console.log(b);
+}
+
+addAll(2,3,4,5,6,7,8);
