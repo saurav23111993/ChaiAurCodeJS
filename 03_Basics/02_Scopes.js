9928d3bdcc8882910b57105c6531cbd2ae412099
saurav23111993 <106614642+saurav23111993@users.noreply.github.com>
2025-02-27 05:18:17 +0000

    Scope

--- 03_Basics/02_Scopes.js
+++ 03_Basics/02_Scopes.js
@@ -22,3 +22,56 @@ console.log(c);
 console.log(d);
 
 // Block Scope and Global Scope
+
+
+//Nested
+
+function one(){
+    const username = "Saurav"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+}
+
+one();
+
+
+if (true){
+    const username = "Sabarna"
+    
+    if(username === "Sabarna"){
+        const website = "Facebook";
+        console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+
+}
+// console.log(username);
+
+function secondaryFunction(num){
+
+    return num +1;
+}
+
+secondaryFunction(5)
+
+/// This is a Function Expression
+// tertiarysum(6); /// Not possible as the function is called before declaration
+const tertiarysum = function(num){
+    return num + 5;
+}
+
+
+/// Arrow Function
+
+
+
+
+
+
