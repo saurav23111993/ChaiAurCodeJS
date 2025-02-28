2522155b2a472e4b904a2ddb7344990e61c762e5
saurav23111993 <106614642+saurav23111993@users.noreply.github.com>
2025-02-28 06:14:21 +0000

    JS Basics

---
+++ 03_Basics/04_Switch.js
@@ -0,0 +1,23 @@
+let key = "Saurav"
+
+
+switch(key){
+
+    case "Saurav":
+    console.log("Saurav");
+    break; /// Break is there to break the flow otherwise everything will run.
+
+    case "Sabarna":
+    console.log("Sabarna");
+    break;
+
+    case "Mummy":
+    console.log("Mummy");
+    break;
+
+    default:
+        console.log("Papa");
+        
+}
+
+
